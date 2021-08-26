7b34013ecfc11d5fc0ea9d606b33841284888753
jay75chauhan <chauhan75jayk@gmail.com>
2021-08-26 10:42:24 +0530

    face mesh added

--- src/App.js
+++ src/App.js
@@ -3,26 +3,56 @@ import * as tf from "@tensorflow/tfjs";
 import * as facemesh from "@tensorflow-models/facemesh";
 import Webcam from "react-webcam";
 import { useRef } from "react";
+import { drawMesh } from "./utilities";
 
 function App() {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
+
+  //load facemesh
 
   const runFacemesh = async () => {
     const net = await facemesh.load({
       inputResolution: { width: 640, height: 480 },
       scale: 0.8,
     });
+    setInterval(() => {
+      detect(net);
+    }, 100);
   };
 
   const detect = async (net) => {
     if (
       typeof webcamRef.current !== "undefined" &&
       webcamRef.current !== null &&
-      webcamRef.current.video.readState === 4
+      webcamRef.current.video.readyState === 4
     ) {
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      //set video width
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+      //set canvas width:
+
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+
+      //make detections
+
+      const face = await net.estimateFaces(video);
+      console.log(face);
+
+      // Get canvas context
+      const ctx = canvasRef.current.getContext("2d");
+      requestAnimationFrame(() => {
+        drawMesh(face, ctx);
+      });
     }
   };
+
+  runFacemesh();
 
   return (
     <div className="App">
